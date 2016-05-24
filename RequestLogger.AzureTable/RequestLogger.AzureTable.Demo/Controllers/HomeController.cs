c8eff22e451691a7edfdb80b0433624e728b3e76
Igor Vinogradov <vse.umrem@hotmail.com>
2016-05-24 15:27:43 +0300

    Demo project

---
+++ RequestLogger.AzureTable/RequestLogger.AzureTable.Demo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RequestLogger.AzureTable.Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var hc = new System.Net.Http.HttpClient(new iflight.RequestLogger.AzureTable.LoggingHandler(new HttpClientHandler()));
+            var t = await hc.GetStringAsync("https://www.example.com");
+
+            return View();
+        }
+
+        public IActionResult Demo()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+    }
+}
